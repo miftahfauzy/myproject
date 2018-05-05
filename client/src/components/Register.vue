9c52767035021ccc03f073b86a08ca0f428c42fd
Miftah Fauzy <miftah.fauzy@gmail.com>
2018-05-06 00:48:11 +0700

    update client script

---
+++ client/src/components/Register.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="register">
+   <h1>Register</h1>
+   <Label>Email: </Label>
+   <input type="email" name="email" placeholder="Please input your email!"><br>
+   <Label>Password: </Label>
+   <input type="password" name="password" placeholder="Please input your password!">
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
